f488db5f017e28968f27f28f8caabfcb43bcce77
mdrafsan <mdrafsan@buffalo.edu>
2017-09-16 04:52:28 -0400

    SignUp

---
+++ FaceQR/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginUI.swift
+//  FaceQR
+//
+//  Created by Rafsan Chowdhury on 9/16/17.
+//  Copyright © 2017 Richard Ni. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
