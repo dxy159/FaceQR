e7f77aef0b14efefb62bd90a6c44570cde700987
mdrafsan <mdrafsan@buffalo.edu>
2017-09-16 03:36:03 -0400

    First Commit

--- FaceQR/ViewController.swift
+++ FaceQR/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // Testing 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
