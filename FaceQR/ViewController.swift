7d18410c2c16b4af00e049a3e2fd899d84eea763
dxy159 <richard.ni.wchs@gmail.com>
2017-09-16 05:44:46 -0400

    finished camera

--- FaceQR/ViewController.swift
+++ FaceQR/ViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+    @IBOutlet weak var imageView: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,60 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    @IBAction func cameraAction(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
+        }
 
-
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+        let imageData = UIImageJPEGRepresentation(imageView.image!, 0.6)
+        let compressImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressImage!, nil, nil, nil)
+        
+    }
+    
+    @IBAction func galleryButton(_ sender: UIButton) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            picker.allowsEditing = true
+            self.present(picker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
 
