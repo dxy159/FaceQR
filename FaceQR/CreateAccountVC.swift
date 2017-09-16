e9bdcdd3b6a87c353a45a981be5c9ffd5079d3d8
mdrafsan <mdrafsan@buffalo.edu>
2017-09-16 10:24:02 -0400

    Halfway done

--- FaceQR/CreateAccountVC.swift
+++ FaceQR/CreateAccountVC.swift
@@ -1,5 +1,5 @@
 //
-//  LoginUI.swift
+//  CreateAccountVC.swift
 //  FaceQR
 //
 //  Created by Rafsan Chowdhury on 9/16/17.
@@ -10,20 +10,33 @@ import UIKit
 import Firebase
 import MobileCoreServices
 
-class LoginVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class CreateAccountVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBOutlet weak var facePic: UIImageView!
-    
-    @IBOutlet weak var verifyButton: RoundButton!
-    private var imagePicker: UIImagePickerController!
+    @IBOutlet weak var takePicButton: UIButton!
+    @IBOutlet weak var faceImage: UIImageView!
 
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var interests: UITextField!
+    @IBOutlet weak var bio: UITextField!
+    
+    @IBOutlet weak var facebook: UITextField!
+    @IBOutlet weak var instagram: UITextField!
+    @IBOutlet weak var linkedin: UITextField!
+    @IBOutlet weak var snapchat: UITextField!
+    @IBOutlet weak var github: UITextField!
+    @IBOutlet weak var twitter: UITextField!
+    
+    private var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func verify(_ sender: RoundButton) {
+
+    @IBAction func takePicPressed(_ sender: Any) {
+        self.takePicButton.isHidden = true
         self.imagePicker = UIImagePickerController()
         self.imagePicker.delegate = self
         
@@ -37,26 +50,27 @@ class LoginVC: UIViewController, UIImagePickerControllerDelegate, UINavigationCo
         
         //self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: imagePicker.sourceType)!
         self.present(imagePicker, animated: true, completion: nil)
-        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        self.facePic.layer.masksToBounds = true
-        self.facePic.layer.cornerRadius = 10
+        self.faceImage.layer.masksToBounds = true
+        self.faceImage.layer.cornerRadius = 10
         let mediaType = info[UIImagePickerControllerMediaType] as? String
         if mediaType == (kUTTypeImage as String) {
             // A PHOTO WAS TAKEN
-           
-            self.facePic.image = info[UIImagePickerControllerOriginalImage] as? UIImage // CANT CAST AS EXPLICITLY
-            self.verifyButton.setTitle("Verifying...", for: .normal)
             
-            // Do THE VERIFICATION HERE 
-        
+            self.faceImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage // CANT CAST AS EXPLICITLY
+            
+            // Do THE VERIFICATION HERE
+            
         } else {
             // A VIDEO WAS SHOT
         }
         
         self.dismiss(animated: true, completion: nil)
     }
-
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        
+    }
 }
